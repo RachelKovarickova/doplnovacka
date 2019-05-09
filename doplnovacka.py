f9943b8b47d17ca33dc14fcb3680a5c6b6ad5e4f
Rachel Kovarickova <rachelkovarickova@icloud.com>
2019-05-09 23:38:19 +0200

    update, moznost nahrani dokumentu i textu

--- doplnovacka.py
+++ doplnovacka.py
@@ -1,5 +1,5 @@
 # doplnovacka
-
+"""
 with open ("lorem.txt") as f:
 	text = f.read()
 f.closed
@@ -29,3 +29,49 @@ for i in range (0, len(seznam)):
 s = " ".join(vysledek) # vsechny prvky vysledku spoj pomoci mezery
 s = s.replace("#", "\n")
 print(s)
+"""
+
+import re
+import string
+
+
+file_text = input('Vlož text nebo soubor s textem i s příponou: ')
+
+text_erase = int(input('Kolikáte slovo smažeme? '))
+
+
+def nahrazeni():
+
+	roster = []
+	output = []
+	roster = text.split(' ')
+	
+	for i in range(0, len(roster)):
+		word = roster[i]
+		changed_word = ''
+
+		if ((i + 1) % text_erase == 0):
+			for letter in word:
+				if letter in string.ascii_letters:
+					changed_word += '*'
+				else:
+					changed_word += letter
+			output.append(changed_word)
+		else:
+			output.append(word)
+
+	s = ' '.join(output)
+	print(s)
+
+
+if re.fullmatch('.+\.txt$', file_text):
+	with open(file_text) as f:
+		text = f.read()
+	f.closed
+	nahrazeni()
+else:
+	text = file_text
+	nahrazeni()
+	
+
+
